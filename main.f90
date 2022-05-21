b4ad07a76e08ba055304682a97f5b44abf08d6c2
ChronosLim <94168703+ChronosLim@users.noreply.github.com>
2022-05-21 20:54:02 +0800

    Create main.f90
    Where we start

---
+++ main.f90
@@ -0,0 +1,15 @@
+programm main
+  implicit none
+  real*8 ::pi=4.0de*atan(1.0d0),L=1.0d0,dx,dt,tend,alpha
+
+!Select one preseted grid densities or set your onw one as your like to do experiment.
+  
+  integer,paramater ::N=32
+!  integer,paramater ::N=64
+!  integer,paramater ::N=128
+!  integer,paramater ::N=256
+
+
+
+
+end programm
